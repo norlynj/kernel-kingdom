baade62151b1042970b49fa3b5c6a1db3260fdb9
norlynj <nacastillo2@up.edu.ph>
2023-02-28 23:39:58 +0800

    Add menu panel

--- src/view/KernelKingdom.java
+++ src/view/KernelKingdom.java
@@ -1,6 +1,26 @@
+import javax.swing.*;
+
 public class KernelKingdom {
     private final Frame frame;
+    private Panel menuPanel;
     public KernelKingdom() {
-        frame = new Frame("Interstellar");
+        frame = new Frame("Kernel Kingdom");
+
+        // MAIN MENU PANEL
+        menuPanel = new Panel("menu.png");
+        JButton startButton = new Button("start");
+        JButton instructionsButton = new Button("instructions");
+        JButton settingsButton = new Button("settings");
+
+        startButton.setBounds(74, 300, 200, 70);
+        instructionsButton.setBounds(74, 400, 200, 70);
+        settingsButton.setBounds(74, 500, 200, 70);
+
+        menuPanel.add(startButton);
+        menuPanel.add(instructionsButton);
+        menuPanel.add(settingsButton);
+
+        frame.add(menuPanel);
+        frame.setVisible(true);
     }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worlgggd!");
         KernelKingdom kk = new KernelKingdom();
-        System.exit(0);
+//        System.exit(0);
     }
 }
