50722c256ea964a081db3e622037a4c9fea8f34f
norlynj <nacastillo2@up.edu.ph>
2023-03-03 17:18:29 +0800

    [WIP] Add keyboard buttons

---
+++ src/view/ImageButton.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+//class for making an image button
+public class ImageButton extends JButton {
+    public ImageButton(String imageName) {
+        this(imageName, 300, 70);
+    }
+
+    public ImageButton(String imageName, int width, int height) {
+
+        setBorder(BorderFactory.createEmptyBorder());
+        setPreferredSize(new Dimension(width, height));
+        setSize(width, height);
+        setBackground(new Color(0, 0, 0, 0));
+        setOpaque(false);
+        setIcon(new ImageIcon(Objects.requireNonNull("src/resources/images/buttons/keyboard/" + imageName))); //img bg
+        setVisible(true);
+
+        addActionListener(e -> {
+//            new AudioPlayer("click.wav").play(); //click sound
+            System.out.println("click");
+        });
+
+    }
+
+}
