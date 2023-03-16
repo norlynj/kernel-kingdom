add3cae6ca93058c9503a3a42141441a04a21b4d
norlynj <nacastillo2@up.edu.ph>
2023-03-16 12:04:48 +0800

    [fix bug] keylistener not working - set button focusable to false

--- src/com/kernelkingdom/view/component/ImageButton.java
+++ src/com/kernelkingdom/view/component/ImageButton.java
@@ -26,6 +26,7 @@ public class ImageButton extends JButton {
         setOpaque(false);
         this.setIcon(imageName);
         setVisible(true);
+        setFocusable(false);
 
         addActionListener(e -> {
             new AudioPlayer("click.wav").play(); //click sound
